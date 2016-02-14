56eb676ae9afd36f307f78a1b022717a321cd110
Kyle Rames <kyle@rames.org>
2016-02-14 08:06:00 -0600

    adding my theme; updating brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -4,16 +4,17 @@ sudo -v # ask for password only at the beginning
 
 brew update
 brew upgrade
+brew tap tmate-io/tmate
 #brew tap homebrew/versions
-brew tap phinze/homebrew-cask
+brew tap caskroom/cask
 
 brew install brew-cask
 brew install emacs --use-git-head --cocoa -srgb
 brew install gist
 brew install hub
-brew install mysql
-brew install postgres
-brew install sqlite
+#brew install mysql
+#brew install postgres
+#brew install sqlite
 brew install ssh-copy-id
 brew install tmux
 brew install chruby
@@ -21,15 +22,16 @@ brew install ruby-install
 
 
 #install textual and xcode
-brew cask install adium
-brew cask install gitx-rowanj
-brew cask install google-chrome 
-brew cask install hipchat
+#brew cask install adium
+brew cask install gitx
+brew cask install google-chrome
+#brew cask install hipchat
 brew cask install iterm2
 brew cask install keyremap4macbook
 brew cask install mou
 brew cask install onepassword
 brew cask install spotify
 #brew cask install virtualbox
+brew cask install dockertoolbox
 
 
---
+++ krames.zsh-theme
@@ -0,0 +1,4 @@
+PROMPT='%{$fg[gray]%}%m%{$reset_color%}:%{$fg_bold[yellow]%}%c%{$reset_color%}$(git_prompt_info) %(!.#.$) '
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[cyan]%}["
+ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%}"
